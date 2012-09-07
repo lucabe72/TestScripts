b68319a7e72c46c30294c5b54644ac4d1ec86604
Luca Abeni <luca.abeni@unitn.it>
2012-09-07 11:13:00 +0200

    Add another test script by Csaba

---
+++ test-cont.sh
@@ -0,0 +1,27 @@
+RES=---
+read_pkts() {
+  TMP=`/sbin/ifconfig -s eth1 | grep eth1`
+  RES=`echo $TMP | cut -d ' ' -f $1`
+}
+
+RATE=$1
+sudo bash pktgen-test.cfg $RATE &
+PID=$!
+
+while sudo kill -0 $PID
+ do
+  read_pkts 4
+  PREV=$RES
+  read_pkts 6
+  PREVE=$RES
+  sleep 1
+  read_pkts 4
+  NEXT=$RES
+  read_pkts 6
+  NEXTE=$RES
+  RECV=`echo $NEXT - $PREV | bc`
+  ERRS=`echo $NEXTE - $PREVE | bc`
+  PPS=`sudo grep pps /proc/net/pktgen/eth1`
+  DATE=`date --rfc-3339=sec`
+  echo Next: $NEXT - Prev: $PREV = Recv: $RECV
+ done
