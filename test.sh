0917ce66b4ac682d257f579034bf81b286ee5073
Luca Abeni <luca.abeni@unitn.it>
2012-09-21 08:38:41 +0200

    Parametrise the number of packets

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 RES=---
 CARD=eth1
+PKTS=10000000
+
 read_pkts() {
   TMP=$(/sbin/ifconfig -s $CARD | grep $CARD)
   RES=$(echo $TMP | cut -d ' ' -f $1)
@@ -12,7 +14,7 @@ PREVE=$RES
 echo Prev: $PREV  PrevE: $PREVE
 
 /sbin/ifconfig -s $CARD >> results
-sudo bash pktgen-test.cfg -r $1
+sudo bash pktgen-test.cfg -r $1 -p $PKTS
 /sbin/ifconfig -s $CARD >> results
 
 read_pkts 4
@@ -25,9 +27,9 @@ PPS=$(sudo grep pps /proc/net/pktgen/$CARD)
 DATE=$(date --rfc-3339=sec)
 echo Next: $NEXT - Prev: $PREV = Recv: $RECV
 SENTPPS=$1
-echo $RECV/10000000*$SENTPPS
-echo "($RECV*$SENTPPS)/10000000" | bc
-PPS=$(((RECV*SENTPPS)/10000000))
+echo $RECV/$PKTS*$SENTPPS
+echo "($RECV*$SENTPPS)/$PKTS" | bc
+PPS=$(((RECV*SENTPPS)/PKTS))
 echo PPS: $PPS
 echo Recv: $RECV Errs: $ERRS $PPS $DATE >> res.txt
 sudo cat /proc/net/pktgen/$CARD >> results
