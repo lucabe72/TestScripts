d62f8c025d121211712615507b8070d00c32ac2c
Csaba Kiraly <kiraly@disi.unitn.it>
2013-01-30 14:16:10 +0100

    process_and_plot: fix issue of rewriting res.txt

--- process_and_plot.sh
+++ process_and_plot.sh
@@ -1,7 +1,8 @@
 BINDIR=`dirname $0`
+PREFIX="res-"
 
-for file in `ls res*.txt`; do
-  [ -f $dir ] && sh $BINDIR/process.sh $@ $file > ${file/res-/}
+for file in `ls $PREFIX*.txt`; do
+  [ -f $dir ] && sh $BINDIR/process.sh $@ $file > ${file/$PREFIX/}
 done
 
 cat <<END >tmp.gnu
@@ -12,8 +13,8 @@ set key left
 plot x
 END
 
-for file in `ls res*.txt`; do
-  echo 'replot "'${file/res-/}'" u 1:2:3 w yerrorlines t "'${file/res-/}'"' >>tmp.gnu
+for file in `ls $PREFIX*.txt`; do
+  echo 'replot "'${file/$PREFIX/}'" u 1:2:3 w yerrorlines t "'${file/$PREFIX/}'"' >>tmp.gnu
 done
 
 cat <<END >>tmp.gnu
